7f5e16229c12b741111b243f115e835171c50f53
IlhamBinyetov <binyetov631@gmail.com>
2023-05-02 18:01:20 +0400

    using useState Hook in  App.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,21 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
+
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css'
 
-const Cart = props =>{
+const Cart = (props) =>{
 
-    const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name:'Sushi', amount:2, price:12.99}].map((item) => <li>{item.name}</li>)}</ul>;
+    const cartItems = (
+            <ul className={classes['cart-items']}>
+        {[{id: 'c1', name:'Sushi', amount: 2, price: 12.99}].map((item) => (
+        <li>{item.name}</li>
+        ))}
+        </ul>
+        );
 
     return(
-        <Fragment>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -15,11 +23,11 @@ const Cart = props =>{
 
             </div>
             <div className={classes.actions}>
-                <button className={classes['button-alt']}>Close</button>
+                <button className={classes['button--alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
             
-        </Fragment>
+        </Modal>
     );
 };
 
