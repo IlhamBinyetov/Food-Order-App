49efe90bf9cbaa84afcd371a547c99ec6accb356
IlhamBinyetov <binyetov631@gmail.com>
2023-05-02 11:26:52 +0400

    Add new Cart Component and its Css file

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -6,9 +6,9 @@ const MealItemForm = (props) => {
     return (
     <form className={classes.form}>
         <Input label="Amount" input={{
-            id:'amount',
+            id:'amount_' + props.id,
             type:'number',
-            min:'10',
+            min:'1',
             max:'5',
             step:'1',
             defaultValue:'1'
