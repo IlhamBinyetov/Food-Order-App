65262d298f053e00e624967299af5a7f7c1ebf14
IlhamBinyetov <binyetov631@gmail.com>
2023-05-03 16:45:49 +0400

    using useState in Project (HeadercartButton component)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React , { Fragment, useState } from 'react';
+import React , {useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
 
@@ -18,7 +19,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       { cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
@@ -26,7 +27,7 @@ function App() {
       </main>
       
     
-    </Fragment>
+    </CartProvider>
   );
 }
 
